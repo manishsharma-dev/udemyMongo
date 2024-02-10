6de5cf3ea213f1fe76a16278d920ee207d3cf3be
Manish Sharma <sharmakmanish1@gmail.com>
2024-02-10 22:56:05 +0530

    Skip and limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,17 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of database', () => {
-    let jaya
+    let jaya, manish, joe, zach
     beforeEach((done) => {
         jaya = new User({ name: 'jaya' });
-        jaya.save()
-            .then((response) => { done(); })
+        manish = new User({ name: 'manish' });
+        joe = new User({ name: 'joe' });
+        zach = new User({ name: 'zach' });
+        Promise.all([jaya.save(), zach.save(), manish.save(), joe.save()])
+            .then(() => {
+                done();
+            })
+
     })
 
     it('finds all users with name of jaya', (done) => {
@@ -21,7 +27,21 @@ describe('Reading users out of database', () => {
             assert(user.name === 'jaya');
             done();
         })
-
-
     })
-})
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                console.log(users);
+                 assert(users.length === 2);
+                assert(users[0].name === 'joe');
+                assert(users[1].name === 'manish');
+                done();
+            })
+    })
+
+
+});
